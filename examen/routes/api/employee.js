34b8955595694cf2ff9448871b3c02bcf9a9dbea
eguigurens91 <eguigurenmariel@gmail.com>
2019-03-12 19:59:06 -0600

    metodos

--- examen/routes/api/employee.js
+++ examen/routes/api/employee.js
@@ -3,30 +3,94 @@ var router = express.Router();
 
 function initEmployee(db) {
   var empModel = require('./employeeModel')(db);
+  var mongoModel = require('./mongoModel')
 
   //rutas a implementar
   // metodo     ruta                     body
   /*
-      GET       /all
-      GET       /byid/:id
-      GET       /bycompany/:company
-      GET       /byagerange/:min/:max
-      GET       /bytag/:tag
-      POST      /addtag/:id              tag
-      DELETE    /delete/:id
+      GET       /all ///// Ya..
+      GET       /byid/:id ///ya
+      GET       /bycompany/:company ///ya 
+      GET       /byagerange/:min/:max  ///NO se!
+      GET       /bytag/:tag ///ya
+      POST      /addtag/:id ///ya              tag
+      DELETE    /delete/:id //// ya
       POST      /makeolder               age
    */
+  var employeed = {
+    '_id':'',
+    'Nombre':'',
+    'phone':null,
+    'email':'',
+    'type': 'small' 
+  };
+  router.get('/all', function( req, res, next) {
+    if(!data){
+      fileModel.read(function(err, filedata){
+        if(err){
+          console.log(err);
+          data = [];
+          return res.status(500).json({'error':'Error al obtener la data requerida.'});
+        }
+        data = JSON.parse(filedata);
+        return res.status(200).json(data);
+      });
+    } else {
+      return res.status(200).json(data);
+    }
+  });
 
-  router.get('/all', (req, res, next) => {
-    /*
-    empModel.xyz( (err, docs)=>{
-      return res.status(200).json(docs);
-    });
-    */
-  });// all
+  router.get('/byid/:id', (req, res, next)=>{
+    mongoModel.getEmploById(req.params.Emploid, (err, emploDoc)=>{
+      if(err){
+        console.log(err);
+        return res.status(500).json({"error":"Error al obtener el empleado"});
+      }
+      return res.status(200).json(thingDoc);
+    } );
+  }); 
 
+  router.get('/bycompany/:company', (req, res, next)=>{
+    mongoModel.getEmployeesByCompany(req.params.companyId, (err, companyDoc)=>{
+      if(err){
+        console.log(err);
+        return res.status(500).json({"error":"Error al obtener el empleado por compania"});
+      }
+      return res.status(200).json(employeedDoc);
+    } );
+  }); 
+
+  router.get('/bytags/:tag', (req, res, next)=>{
+    mongoModel.searchByTag((req.params.tag || '').split('_'), (err, docs)=>{
+      if(err){
+        console.log(err);
+        return res.status(500).json({"error":"No se encontro la data"});
+      }else{
+        return res.status(200).json(docs);
+      }
+    } ); 
+  }); 
+  
+  router.delete('/delete/:employeedId', function(req, res, next){
+    var _employeedId = req.params.employeedId;
+    mongoModel.deleteById(_employeedId, (err, result)=>{
+      if(err){
+        return res.status(500).json({"error":"No se pudo eliminar dato"});
+      }
+      return res.status(200).json(result);
+    }); 
+  }); 
+  
+  fileModel.read(function(err , filedata){
+    if(err){
+      console.log(err);
+    } else{
+      data = JSON.parse(filedata);
+    }
+  });
   
   return router;
 }
 
 module.exports = initEmployee;
+
--- examen/routes/api/employeeModel.js
+++ examen/routes/api/employeeModel.js
@@ -2,63 +2,75 @@ var ObjectID = require('mongodb').ObjectID;
 
 function employeeModel(db){
   var lib = {};
-  var empColl = db.collection('emps');
+  var empColl = db.collection('empleado');
+  
   lib.getEmployees = (handler)=>{
-    // implementar
-    // obtener todos los documentos
-    return handler(new Error("No Implementado"), null);
+    empColl.find({}).toArray(
+      (err , docs) => {
+        if(err){
+          handler(err, null);
+        }else{
+          handler(null, docs);
+        }
+      }
+     ); 
   }
 
+
   lib.getEmployeesById = (id, handler) => {
-    // implementar
-    // Obtener un Documento solo mostrar
-    // email, phone, name y age
-    return handler(new Error("No Implementado"), null);
+  empColl.findOne({ "_id": new ObjectId(empleadoId)}, (err, doc)=>{
+      if(err){
+        handler(err, null);
+      }else{
+        handler(null, doc);
+      }
+    });
   }
 
   lib.getEmployeesByCompany = (company, handler) => {
-    // implementar
-    // solo mostrar name, email, company
-    return handler(new Error("No Implementado"), null);
-  }
-
-  lib.getEmployeesByAgeRange = (ageLowLimit, ageHighLimit, handler) => {
-    //implementar
-    // Mostrar todos los documento incluyendo los extremos
-    // que esten entre las edades indicadas por los parametros
-    // ageLowLimit y ageHighLimit
-    // solo mostrar name, age, email
-    return handler(new Error("No Implementado"), null);
+    empColl.findOne({"_idcompany":new ObjectID(companyId)},(err,doc)=>{
+      if (err){
+        handler(err,null);
+      }else{
+        handler(null,doc);
+      }
+    });  
   }
 
   lib.getEmployeesByTag = (tag, handler) => {
-    //implementar
-    // obtener todos los documentos que contenga 
-    // al menos una vez el tag dentro del arreglo
-    // tags
-    // mostrar solo name, email, tags
-    return handler(new Error("No Implementado"), null);
+    var queryObject= {"tags": {"$in": Array.isArray(tags)? tags: [tags]}};
+    empColl.find(queryObject).toArray((err, docs) => {
+      if(err){
+        handler(err, null);
+      }else{
+        handler(null, docs);
+      }
+    }); 
   }
 
   lib.addEmployeeATag = ( tag, id, handler) => {
-    //Implementar
-    //Se requiere agregar a un documento un nuevo tag
-    // $push
-    return handler(new Error("No Implementado"), null);
+    var curatedTags = Array.isArray(tags)? tags: [tags];
+    var updateObject = { "$set": { "tags": curatedTags}};
+    empColl.updateOne({"_id": ObjectId(id)}, updateObject, (err, rsult)=>{
+        if(err){
+          handler(err, null);
+        }else{
+          handler(null, rsult.result);
+        }
+    } ); 
   }
 
   lib.removeEmployee = (id, handler) => {
-    //Implementar
-    //Se requiere eliminar un documento de la colección
-    return handler(new Error("No Implementado"), null);
-  }
+      empColl.deleteOne({"_id": ObjectId(Id)}, (err, rslt)=>{
+      if(err){
+        console.log(err);
+        handler(err, null);
+      } else {
+        handler(null, rslt.result);
+      }
+    }); 
+     }
 
-  lib.increaseAgeToAll = (ageDelta, handler) => {
-    //Implementar
-    //Se requiere modificar todos los documentos de la colección
-    // incrementando age por la cantidad de ageDelta $inc
-    return handler(new Error("No Implementado"), null);
-  }
   return lib;
 }
 
